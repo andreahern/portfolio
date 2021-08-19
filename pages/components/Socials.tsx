5f9d2060d344b628e8252fdc78cfaf20f4cd65b5
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-08-18 23:30:28 -0400

    Update social logos.

--- pages/components/Socials.tsx
+++ pages/components/Socials.tsx
@@ -1,28 +1,23 @@
-import Image from "next/image";
 import Link from "next/link";
-import githubLogo from "../../public/github.png";
-import linkedinLogo from "../../public/linkedin.png";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Socials() {
   return (
-    <div className="fixed bottom-0 right-0 mr-8 space-x-4">
+    <div className="fixed bottom-2 right-2 md:right-4 w-16 h-10 md:w-24 md:h-20 space-x-4 flex flex-row">
       <Link href="https://github.com/habraham2023">
-        <a>
-          <Image
-            src={githubLogo}
-            alt="Github profile link"
-            width={32}
-            height={32}
+        <a className="w-1/2 h-full flex items-end justify-end">
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="hover:text-gray-500 text-gray-900"
           />
         </a>
       </Link>
       <Link href="https://www.linkedin.com/in/abeher/">
-        <a>
-          <Image
-            src={linkedinLogo}
-            alt="Linkedin profile link"
-            width={32}
-            height={32}
+        <a className="w-1/2 h-full flex items-end justify-end">
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className="hover:text-gray-500 text-gray-900"
           />
         </a>
       </Link>
